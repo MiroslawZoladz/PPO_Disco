486cd50a1b858c65408abf6a8a0b9973a58077bd
Miro <zoladz@agh.edu.pl>
2024-11-19 12:42:25 +0100

    1c. changing led

--- BSP/src/main.cxx
+++ BSP/src/main.cxx
@@ -1,21 +1,16 @@
-//1b. Zmodyfikowac program tak aby dioda zapalala sie na 0.1s co 1s
-//
-// Użyc funkcji do ustawiania stanu wysoiego i niskiego na ledach
-// W tym celu znaleźc deklarację BSP_LED_Toggle:
-// - (menu kontekstowe na nazwie funkcji: "Open Declaration")
-// - poszukac w pliku który się otworzy
-// - jka sie nazywa ten plik i co ma wspólnego z plikiem main.cxx?
+//1c. Zmienić diodę na inną dostepną na płytce
+// Nazwe diody znaeźć w sposób analogiczny ja nazwe funkcji w poprzednim zadaniu
 
 #include <stm32f4xx_hal.h>
 #include <stm32f429i_discovery.h>
 
 int main(void)
 {
-	BSP_LED_Init(LED3);
+	BSP_LED_Init(LED4);
 	while (true) {
-		BSP_LED_On(LED3);
+		BSP_LED_On(LED4);
 		HAL_Delay(100);
-		BSP_LED_Off(LED3);
+		BSP_LED_Off(LED4);
 		HAL_Delay(900);
 	}
 }
